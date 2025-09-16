243cfb1d5f09324cf3606a9a5aad8a0633fed964
fcongedo <francocongedo@gmail.com>
2025-09-15 21:38:18 -0300

    Agrego ejercicio terminados

--- guia-pilas-y-colas-fcongedo/ejercicios/ejercicios.go
+++ guia-pilas-y-colas-fcongedo/ejercicios/ejercicios.go
@@ -1,12 +1,23 @@
 package ejercicios
 
-import "untref-ayp2/guia-pilas-colas/queue"
+import (
+	"untref-ayp2/guia-pilas-colas/queue"
+	"untref-ayp2/guia-pilas-colas/stack"
+)
 
 // Escribir una función que que reciba una cadena de caracteres
 // y devuelva la cadena invertida. Analizar el orden.
 func InvertirCadena(cadena string) string {
-	// Implementar
-	return ""
+	pila := stack.NewStack[rune]()
+	for _, letra := range cadena {
+		pila.Push(letra)
+	}
+	invertida := make([]rune, 0, len(cadena))
+	for !pila.IsEmpty() {
+		letra, _ := pila.Pop()
+		invertida = append(invertida, letra)
+	}
+	return string(invertida)
 }
 
 // Escribir una función que verifique si una palabra es palíndromo
@@ -14,8 +25,20 @@ func InvertirCadena(cadena string) string {
 //
 //	las cadenas "1456541" y "145541" son palíndromos). Analizar el orden.
 func Palindromo(cadena string) bool {
-	// Implementar
-	return false
+	pila := stack.NewStack[rune]()
+
+	for _, letra_original := range cadena {
+		pila.Push(letra_original)
+	}
+
+	for _, letra_original := range cadena {
+		letra_pila, _ := pila.Pop()
+		if letra_original != letra_pila {
+			return false
+		}
+	}
+
+	return true
 }
 
 // Escribir una función que evalúe si una cadena de paréntesis, corchetes y
@@ -23,8 +46,39 @@ func Palindromo(cadena string) bool {
 // `false` si está mal balanceada. Por ejemplo: `[()]{}{[()()]()}` debe
 // devolver `true`, mientras que `[(])` debe devolver `false`. Analizar el orden.
 func Balanceada(cadena string) bool {
-	// Implementar
-	return false
+	pila := stack.NewStack[string]()
+	for _, caracter := range cadena {
+		switch caracter {
+		case '(', '[', '{':
+			pila.Push(string(caracter))
+		case ')':
+			if pila.IsEmpty() {
+				return false
+			}
+			tope, _ := pila.Pop()
+			if tope != "(" {
+				return false
+			}
+		case '}':
+			if pila.IsEmpty() {
+				return false
+			}
+			tope, _ := pila.Pop()
+			if tope != "{" {
+				return false
+			}
+		case ']':
+			if pila.IsEmpty() {
+				return false
+			}
+			tope, _ := pila.Pop()
+			if tope != "[" {
+				return false
+			}
+		}
+
+	}
+	return pila.IsEmpty()
 }
 
 // Escribir una función, tal que, dadas dos colas, construya una cola con el resultado
@@ -32,6 +86,17 @@ func Balanceada(cadena string) bool {
 // frente y 3 al final) y `q2 = [5,7]`, el resultado es `[1,2,3,5,7]` (con 1 al
 // frente y 7 al final). Analizar el orden.
 func UnirColas[T any](q1, q2 *queue.Queue[T]) *queue.Queue[T] {
-	// Implementar
-	return nil
+	nueva_cola := queue.NewQueue[T]()
+
+	for !q1.IsEmpty() {
+		elemento, _ := q1.Dequeue()
+		nueva_cola.Enqueue(elemento)
+	}
+
+	for !q2.IsEmpty() {
+		elemento, _ := q2.Dequeue()
+		nueva_cola.Enqueue(elemento)
+	}
+
+	return nueva_cola
 }
--- guia-pilas-y-colas-fcongedo/stackqueue/stackqueue.go
+++ guia-pilas-y-colas-fcongedo/stackqueue/stackqueue.go
@@ -1,37 +1,63 @@
 package stackqueue
 
+import (
+	"fmt"
+	"untref-ayp2/guia-pilas-colas/stack"
+)
+
 // Implementación de una cola genérica utilizando dos pilas
 type StackQueue[T any] struct {
-	// Implementar
+	stackIn  *stack.Stack[T]
+	stackOut *stack.Stack[T]
 }
 
 // Crea una nueva cola vacía.
 func NewStackQueue[T any]() *StackQueue[T] {
-	// Implementar
-	return nil
+	return &StackQueue[T]{
+		stackIn:  stack.NewStack[T](),
+		stackOut: stack.NewStack[T](),
+	}
 }
 
 // Agrega un elemento a la cola.
 func (q *StackQueue[T]) Enqueue(v T) {
-	// Implementar
+	q.stackIn.Push(v)
 }
 
 // Elimina y devuelve el elemento al frente de la cola.
 func (q *StackQueue[T]) Dequeue() (T, error) {
-	// Implementar
 	var x T
-	return x, nil
+	if q.stackOut.IsEmpty() {
+		for !q.stackIn.IsEmpty() {
+			v, _ := q.stackIn.Pop()
+			q.stackOut.Push(v)
+		}
+	}
+	if q.stackOut.IsEmpty() {
+		return x, fmt.Errorf("cola vacía")
+	}
+	return q.stackOut.Pop()
 }
 
 // Devuelve el elemento al frente de la cola.
 func (q *StackQueue[T]) Front() (T, error) {
 	// Implementar
 	var x T
-	return x, nil
+	if q.stackOut.IsEmpty() {
+		for !q.stackIn.IsEmpty() {
+			v, _ := q.stackIn.Pop()
+			q.stackOut.Push(v)
+		}
+	}
+	if q.stackOut.IsEmpty() {
+		return x, fmt.Errorf("cola vacía")
+	}
+	return q.stackOut.Top()
+
 }
 
 // Devuelve true si la cola está vacía.
 func (q *StackQueue[T]) IsEmpty() bool {
-	// Implementar
-	return false
+
+	return q.stackIn.IsEmpty() && q.stackOut.IsEmpty()
 }
